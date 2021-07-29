96d3a917757802c20a57efd94c8eae7d22d2b7dc
Nestor Otondo <nestor.otondo@gmail.com>
2021-07-29 00:09:20 -0400

    add geolocation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-const App = () => {
-  return <><h2>Weather App</h2></>
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+    });
+  }
+
+  render() {
+    return <>
+      <h2>Weather App</h2>
+      <label>Your city:</label>
+      <input name="city" />
+    </>
+  }
 }
 
 export default App;
