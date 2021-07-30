f9e9bdbecf0ce377796bdeda9b877bf0a28c42cd
Nestor Otondo <nestor.otondo@gmail.com>
2021-07-30 19:35:24 -0400

    create env variables

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+const API_URL = 'https://maps.googleapis.com/maps/api/geocode/json?latlng';
+
 class App extends Component {
 
   constructor(props) {
@@ -12,10 +15,8 @@ class App extends Component {
   }
 
   componentDidMount() {
-    const APIurl = 'https://maps.googleapis.com/maps/api/geocode/json?latlng';
-    const key = 'AIzaSyDJQ-1MFRpd5pYHua5TSqlNvxGmCezQ9j4';
     const fetchLocation = async (position) => {
-      const res = await Axios(`${APIurl}=${position.coords.latitude},${position.coords.longitude}&sensor=true&key=${key}`);
+      const res = await Axios(`${API_URL}=${position.coords.latitude},${position.coords.longitude}&sensor=true&key=${API_KEY}`);
       this.setState({ data: res.data['results'] });
       this.setState({ long_name: this.state.data[0]['address_components'][1]['long_name'] });
     }
