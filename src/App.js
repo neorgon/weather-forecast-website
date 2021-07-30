a582e467ec13e156c2574e7a9eca58743e3ea5ef
Nestor Otondo <nestor.otondo@gmail.com>
2021-07-30 17:47:56 -0400

    remove key api

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     super(props);
     this.state = {
       data: null,
+      long_name: null
     };
   }
 
@@ -15,7 +16,8 @@ class App extends Component {
     const key = 'AIzaSyDJQ-1MFRpd5pYHua5TSqlNvxGmCezQ9j4';
     const fetchLocation = async (position) => {
       const res = await Axios(`${APIurl}=${position.coords.latitude},${position.coords.longitude}&sensor=true&key=${key}`);
-      this.setState({ data: res.data });
+      this.setState({ data: res.data['results'] });
+      this.setState({ long_name: this.state.data[0]['address_components'][1]['long_name'] });
     }
     navigator.geolocation.getCurrentPosition(fetchLocation);
   }
@@ -24,7 +26,7 @@ class App extends Component {
     return <>
       <h2>Weather App</h2>
       <label>Your city:</label>
-      <input name="city" defaultValue={this.state.data ? this.state.data['results'][0]['address_components'][1]['long_name'] : ''} />
+      <input name="city" defaultValue={this.state.long_name ? this.state.long_name : ''} />
     </>
   }
 
